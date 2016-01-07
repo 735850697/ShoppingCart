3dca7025d8befface442e44b28cf0abb5fe4030e
刘学丽 <15831177042@163.com>
2016-01-07 15:15:15 +0800

    Initial Commit

---
+++ 购物车/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  购物车
+//
+//  Created by beijingduanluo on 16/1/7.
+//  Copyright © 2016年 beijingduanluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 购物车/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  购物车
+//
+//  Created by beijingduanluo on 16/1/7.
+//  Copyright © 2016年 beijingduanluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
